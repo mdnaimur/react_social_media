929adc495a15c017774ec1fbe45e3d4be0755cfd
mdnaimurr <mdnaimurr@gmail.com>
2022-07-02 11:03:57 +0600

    Reacte Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-// import Profile from "./pages/profile/Profile";
-// import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
